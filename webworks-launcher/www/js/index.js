78ddab7b385ff5663f889946f2a8374407e1592f
Chad Tetreault <ctetreault@blackberry.com>
2015-06-08 11:48:36 -0400

    updated to support bb10, android, ios

--- webworks-launcher/www/js/index.js
+++ webworks-launcher/www/js/index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2014 BlackBerry Limited.
+ * Copyright 2015 BlackBerry Limited.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,50 +9,53 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * Author: Dennis Reumer, BlackBerry
- * Date: April 2014
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
+
+/* EDIT THE FOLLOWING VARIABLES */
+var url = 'http://blackberry.com';
+
+/* !!! DO NOT EDIT ANYTHING BELOW HERE !!! */
+/* !!! DO NOT EDIT ANYTHING BELOW HERE !!! */
+/* !!! DO NOT EDIT ANYTHING BELOW HERE !!! */
+var target = '_system';
+
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        //
-        switch (id) {
-            case "deviceready":
-                app.launchWeblink(blackberry.app.authorURL);
-                break;
-            default:
-                //nothing happens
+    onDeviceReady: function () {
+
+        // for bb10 we must use the invoke framework as _system target doesn't open system browser, but in app
+        var isBlackberry = window.blackberry || false;
+
+        // for bb we have to use their own api
+        if (isBlackberry) {
+            blackberry.invoke.invoke({
+                target: "sys.browser",
+                uri: url
+            }, function () {
+                    blackberry.app.exit();
+                }, function () { });
+
+            // the rest, we can use navigator
+            // NOTE: iOS doesn't allow devs to close an app, no way around this, platform limitation
+        } else {
+            var windowRef = cordova.InAppBrowser.open(url, target);
+
+            setTimeout(function () {
+                try {
+                    navigator.app.exitApp();
+                } catch (e) { }
+            }, 500);
         }
     },
-    //The weblink launcher and closer.
-    launchWeblink: function(url) {
-        // open web link in browser
-        blackberry.invoke.invoke({
-            target: "sys.browser",
-            uri: url
-        }, function() {
-            blackberry.app.exit();
-        }, function() {});
-    }
 };
+
+app.initialize();
